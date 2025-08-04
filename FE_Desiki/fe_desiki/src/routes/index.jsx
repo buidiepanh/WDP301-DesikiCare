28cb555d1cb5a53b4aa755fa14090e4696da6819
BuiDiepAnh <buidiepanh0905@gmail.com>
2025-08-04 20:16:58 +0700

    clean up ver 2

--- FE_Desiki/fe_desiki/src/routes/index.jsx
+++ FE_Desiki/fe_desiki/src/routes/index.jsx
@@ -16,30 +16,11 @@ import PaymentReturn from "../pages/user/payment-return/paymentReturn";
 import Profile from "../pages/user/profile/Profile";
 import GamePage from "../pages/user/Game";
 import GamePlayPage from "../pages/user/Game/Play/GamePlayPage";
-
-// Admin pages
-import UserManagement from "../pages/admin/UserManagement";
-import CustomerPoints from "../pages/admin/CustomerPoints";
-import GameManagement from "../pages/admin/GameManagement";
-import ChatbotContent from "../pages/admin/ChatbotContent";
-
-// Manager pages
-import ProductManagement from "../pages/manager/ProductManagement";
-import InventoryManagement from "../pages/manager/InventoryManagement";
-import OrderManagement from "../pages/manager/OrderManagement";
-import Statistics from "../pages/manager/Statistics";
-import CustomerInfo from "../pages/manager/CustomerInfo";
+import QuizPage from "../pages/user/quiz";
 
 // Layouts
 import HeaderSkincare from "../components/Header/HeaderSkincare";
 import FooterSkincare from "../components/Footer/FooterSkincare";
-import AdminLayout from "../layouts/admin/AdminLayout";
-import { getGamesEvent, updateGamePoints } from "../services/apiServices";
-import { IconButton, Badge } from "@mui/material";
-import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
-import { GamesModal } from "./components/gamesModal";
-import toast from "react-hot-toast";
-import QuizPage from "../pages/user/quiz";
 
 function UserRouter() {
   const [isLogin, setIsLogin] = useState(false);
@@ -67,8 +48,8 @@ function UserRouter() {
           <Route path="/blog-grid" element={<BlogGrid />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/payment-return" element={<PaymentReturn />} />
-          {/* <Route path="/game-type/:id" element={<GameTypePage />} /> */}
-          <Route path="/game-event/:id" element={<GamePlayPage />} />
+          <Route path="/mini-games" element={<GamePage />} />
+          <Route path="/mini-games/play" element={<GamePlayPage />} />
           <Route path="/quiz" element={<QuizPage />} />
         </Routes>
       </div>
@@ -77,39 +58,10 @@ function UserRouter() {
   );
 }
 
-function AdminRouter() {
-  return (
-    <Routes>
-      <Route path="/" element={<AdminLayout />}>
-        <Route path="users" element={<UserManagement />} />
-        <Route path="points" element={<CustomerPoints />} />
-        <Route path="game" element={<GameManagement />} />
-        <Route path="chatbot" element={<ChatbotContent />} />
-      </Route>
-    </Routes>
-  );
-}
-
-function ManagerRouter() {
-  return (
-    <Routes>
-      <Route path="/" element={<ManagerLayout />}>
-        <Route path="products" element={<ProductManagement />} />
-        <Route path="inventory" element={<InventoryManagement />} />
-        <Route path="orders" element={<OrderManagement />} />
-        <Route path="statistics" element={<Statistics />} />
-        <Route path="customers" element={<CustomerInfo />} />
-      </Route>
-    </Routes>
-  );
-}
-
 export function AppRouter() {
   return (
     <Routes>
       <Route path="/*" element={<UserRouter />} />
-      <Route path="/admin/*" element={<AdminRouter />} />
-      <Route path="/manager/*" element={<ManagerRouter />} />
     </Routes>
   );
 }
